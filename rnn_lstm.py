62992b2e21cab74054f4d6e70d67c7424f3432b1
Tran <Duy.Tran@McKesson.com>
2018-12-11 15:43:34 -0600

    remove matplotlib stuff

--- rnn_lstm.py
+++ rnn_lstm.py
@@ -3,9 +3,7 @@ from keras.models import Sequential
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-%matplotlib inline
-import seaborn as sns; sns.set()
+
 
 data = pd.read_csv("Datasets/Kaggle/spam.csv")
 texts = []
